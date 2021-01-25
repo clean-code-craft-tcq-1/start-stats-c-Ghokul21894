e65deac34d653d59cd01e14d9e0448c81fa79a2e
Ghokul21894 <77478569+Ghokul21894@users.noreply.github.com>
2021-01-25 23:48:47 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -2,12 +2,10 @@
 
 void emailAlerter () {
   emailAlertCallCount = 1;
-  return 0;
 }
 
 void ledAlerter () {
   ledAlertCallCount = 1;
-  return 0;
 }
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats) {
