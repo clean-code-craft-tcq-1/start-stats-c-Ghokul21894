f29c8aaf7d890df97b7ef0e68625db75f34da534
Ghokul21894 <77478569+Ghokul21894@users.noreply.github.com>
2021-01-26 01:20:02 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,13 +1,16 @@
+/**********************Types********************************/
 typedef struct {
     float average;
     float max;
     float min;
 }Stats;
-
-Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
-alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
-
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+
+/********************Protoypes & Resources************************/
+Stats compute_statistics(const float* numberset, int setlength);
+void emailAlerter();
+void ledAlerter();
+alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
