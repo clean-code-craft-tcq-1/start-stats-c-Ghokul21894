e33789bb84bd56459b2032ff5d1a16a3b2fc48ea
Ghokul21894 <77478569+Ghokul21894@users.noreply.github.com>
2021-01-26 00:43:41 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,5 +1,5 @@
 #include "stats.h"
 
 alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
-alerter_funcptr emailAlerter();
-alerter_funcptr ledAlerter();
+void emailAlerter();
+void ledAlerter();
