0ca9787218f6bb8bd15339201c77fecc81a60ae9
Ghokul21894 <77478569+Ghokul21894@users.noreply.github.com>
2021-01-26 00:10:49 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -8,7 +8,7 @@ void ledAlerter () {
   ledAlertCallCount = 1;
 }
 
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats) {
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats) {
   if (maxThreshold > computedStats.max) {
     emailAlerter();
     ledAlerter();
