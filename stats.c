d0c70da675c095e7c3efb26e7cca4f27bb77d8d9
Ghokul21894 <77478569+Ghokul21894@users.noreply.github.com>
2021-01-26 00:05:31 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,21 +1,25 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    Stats s = { 0 };
-    for (int i = 0; i < setlength ; i++) {
-        s.average = s.average + numberset[i];
-    }
-    s.average = s.average / setlength;
-    s.min = s.max = numberset[0];
-    for (int i = 1 ; i < setlength ; i++) {
-        if (s.min > numberset[i]) {
-            s.min = numberset[i];
+Stats compute_statistics(const float* numberset, int setlength) {
+	Stats s;
+	s.average = 0;
+	s.min = 0;
+	s.max = 0;
+
+	for (int i = 0; i < setlength ; i++) {
+		s.average = s.average + numberset[i];
+	}
+    	s.average = s.average / setlength;
+    	s.min = s.max = numberset[0];
+    	for (int i = 1 ; i < setlength ; i++) {
+        	if (s.min > numberset[i]) {
+            		s.min = numberset[i];
         }
 		if (s.max < numberset[i]) {
-		    s.max = numberset[i];
-        }
-    }
-    return s;
+		    	s.max = numberset[i];
+        	}
+    	}
+    	return s;
 }
 
 int emailAlertCallCount = 0;
